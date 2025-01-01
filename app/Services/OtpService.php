9d0905d3346d409a92aaf12b0d987bc352b8abb9
mohammed <mohammedalrabeai@gmail.com>
2025-01-01 13:03:50 +0300

    fix otp function

--- app/Services/OtpService.php
+++ app/Services/OtpService.php
@@ -7,18 +7,19 @@ use Illuminate\Support\Facades\Http;
 class OtpService
 {
     protected $baseUrl = 'https://login.isender360.com/api/sync/message/send';
+    // protected $profileId = 'aedd0dc2-8453';
     protected $profileId = '35ab7ec0-63dd';
+
 
     public function sendOtp(string $phone, string $message): bool
     {
-        $response = Http::withHeaders([
-            'accept' => 'application/json',
-            'Content-Type' => 'application/json',
-        ])->post($this->baseUrl, [
-            'profile_id' => $this->profileId,
-            'body' => $message,
+        $response = Http::asForm()->post($this->baseUrl.'?profile_id='.$this->profileId, [
+       
             'recipient' => $phone,
+           
+            'body' => $message,
         ]);
+
         return $response->ok();
     }
 }
