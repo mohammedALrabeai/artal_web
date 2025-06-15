abd0e2dad60786bab0c620713a627b2617078cdd
mohammedALrabeai <mohammedalrabeai@gmail.com>
2025-06-15 13:26:52 +0300

    Update AttendanceResource.php

--- app/Http/Resources/AttendanceResource.php
+++ app/Http/Resources/AttendanceResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,7 +13,7 @@ class AttendanceResource extends JsonResource
     {
         return [
             'id'         => $this->id,
-            'date'       => $this->date->toDateString(),
+            'date'       => Carbon::parse($this->date)->toDateString(),
             'status'     => $this->status,
             'check_in'   => $this->check_in,
             'check_out'  => $this->check_out,
