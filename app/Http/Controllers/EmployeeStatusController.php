bd089884aaec85b9676f624d12b368f3ef4a6bb8
mohammedALrabeai <mohammedalrabeai@gmail.com>
2025-08-26 10:50:18 +0300

    Update EmployeeStatusController.php

--- app/Http/Controllers/EmployeeStatusController.php
+++ app/Http/Controllers/EmployeeStatusController.php
@@ -268,7 +268,7 @@ class EmployeeStatusController extends Controller
     {
         $enabledShifts = Shift::with(['zone.pattern'])
             ->where('status', true)
-            // ->where('exclude_from_auto_absence', false)
+            ->where('exclude_from_auto_absence', false)
             ->get();
 
         $activeShiftIds = [];
